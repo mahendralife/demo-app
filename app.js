1662ac4d8f048097610703f3941ef51bb7d162cb
Mahendra Singh <mahendra.life@hotmail.com>
2016-09-29 11:12:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 var REST_API_URL="API/get_list.php";
 var app=new angular.module("demo",['ngMessages']);
 
+//quick start application
 app.run(function($log){
     $log.info("Application is ready to work");
 })
 
-app.directive("graph", function(http,$timeout){
-  return {
-      scope:{
-        data:'='
-      },
-      link: function(scope,ele,atts){
-        console.log(http.graph(scope.data));
-      //  ele.highcharts(http.graph(scope.data));
-      console.log(atts.id);
-      $timeout(function(){var chart = new Highcharts.Chart(http.graph(scope.data,atts.id));
-        },100);
-      }
-  }
-})
+//controller
 app.controller("formApp", function($scope,http,  $timeout){
   var self=$scope;
   self.data={};
@@ -29,33 +17,10 @@ app.controller("formApp", function($scope,http,  $timeout){
     {"title":"Total projects","view":"3700"},
     {"title":"Completed projects","view":"900"},
     {"title":"Running projects","view":"1700"}];
-  //  console.log(http.storageView("graph"));
+    self.color=['#72d3fe','#45b7af','#ffc66c']
   if(http.storageView("graph")){
     self.graph["percentage"]= http.storageView("graph");
   }
-
-  $timeout(function(){
-        Highcharts.chart('chart0',http.graph(10));
-    //  Highcharts.Chart(http.graph(10,"chart0"));
-},500);
-
-//var chart = new Highcharts.Chart(http.graph(10,"chart0"));
-     //{
-  //     chart: {
-  //         renderTo: 'my-chart'
-  //     },
-  //
-  //     xAxis: {
-  //         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-  //             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  //     },
-  //
-  //     series: [{
-  //         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-  //     }]
-  //
-  // });
-
 
   //get data via REST API
   http.get_data("self.data", REST_API_URL,function(response){
@@ -77,7 +42,7 @@ app.controller("formApp", function($scope,http,  $timeout){
   }
 });
 
-
+//factory services
 app.factory('http',['$http', '$timeout','$templateCache',
 	function($http, $timeout,$templateCache){
 		var services={}
@@ -115,22 +80,16 @@ app.factory('http',['$http', '$timeout','$templateCache',
         }
       }
     }
-    services.graph=function(data){
+    services.graph=function(data,color){
       var config = {
        'chart': {
-           'type': 'solidgauge'
+          'type': 'solidgauge',
+          'backgroundColor':'transparent',
        },
-       'title': null,
-       'tooltip': {
-           'enabled': false
-       },
+
        'pane': {
-         'center': ['50%', '50%'],
-         'size': '200px',
-         'startAngle': 0,
-         'endAngle': 360,
          'background': {
-           'backgroundColor': '#EEE',
+           'backgroundColor': 'white',
            'innerRadius': '90%',
            'outerRadius': '100%',
            'borderWidth': 0
@@ -138,28 +97,18 @@ app.factory('http',['$http', '$timeout','$templateCache',
        },
 
        'yAxis': {
+          'stops': [[0.1, color] ],
          'min': 0,
          'max': 100,
-         'labels': {
-           'enabled': false
-         },
-
-         'lineWidth': 0,
-         'minorTickInterval': null,
-         'tickPixelInterval': 400,
-         'tickWidth': 0
+         'labels': {   'enabled': false },
+         'lineWidth': 0, 'minorTickInterval': null,'tickPixelInterval': 400, 'tickWidth': 0
        },
 
-       'plotOptions': {
-           'solidgauge': {
-               'innerRadius': '90%'
-           }
-       },
-
+      'plotOptions': { 'solidgauge': { 'innerRadius': '90%'}},
+       credits: { enabled: false},
        'series': [{
-           'name': 'Speed',
            'data': [data],
-           'dataLabels': {
+              'dataLabels': {
                'enabled': false
            }
        }]
@@ -169,3 +118,18 @@ app.factory('http',['$http', '$timeout','$templateCache',
     }
 		return services;
 }]);
+
+app.directive('hcChart', function (http) {
+                return {
+                    restrict: 'E',
+                    template: '<div></div>',
+                    scope: {
+                        data: '=',
+                        color:'='
+                    },
+                    link: function (scope, element) {
+
+                        Highcharts.chart(element[0], http.graph(scope.data,scope.color));
+                    }
+                };
+            })
